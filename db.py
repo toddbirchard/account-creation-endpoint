9789fedb0cd6e6031d9d33b2100ef59d8046f57b
Todd Birchard <toddbirchard@gmail.com>
2018-12-02 12:05:14 -0500

    signup form error

--- db.py
+++ db.py
@@ -6,11 +6,16 @@ import os
 class UserAccounts:
     """Create user accounts from submissions."""
 
-    def inser_user_record(self):
+    def __init__(self, username, email, gravatar):
+        self.username = username
+        self.email = email
+        self.gravatar = gravatar
+
+    def insert_user_record(self):
         """Select records for all confirmed new hires which have yet to be onboarded."""
         engine = create_engine(os.environ['URI'], client_encoding="UTF-8")
         meta = MetaData(bind=engine, reflect=True)
         readers_table = meta.tables['readers']
-        ins = readers_table.insert().values(username=username, email=email, gravatar=gravatar)
+        ins = readers_table.insert().values(username=self.username, email=self.email, gravatar=self.gravatar)
         result = engine.connect(ins)
         return result
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mailer import welcome_email
 from gravatar import get_gravatar
 from mailer import welcome_email
 from analytics import mixpanel_profile
+from db import UserAccounts
 
 
 def subscribe(request):
@@ -13,3 +14,6 @@ def subscribe(request):
     # Derive Gravatar from Email
     grav = get_gravatar(email)
     request_json['gravatar'] = grav
+    # Insert DB record
+    new_account = UserAccounts(username, email, grav)
+    new_account.insert_user_record()
