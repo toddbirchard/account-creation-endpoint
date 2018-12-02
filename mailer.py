b70aae730354792fcae2b50b6f96470c52386b36
Todd Birchard <toddbirchard@gmail.com>
2018-12-02 11:51:18 -0500

    sendgrid

--- mailer.py
+++ mailer.py
@@ -1,23 +1,31 @@
 # using SendGrid's Python Library - https://github.com/sendgrid/sendgrid-python
 import sendgrid
+from sendgrid import SendGridAPIClient
 import os
-from sendgrid.helpers.mail import *
+from sendgrid.helpers.mail import Email, Substitution, Mail, Personalization
+from python_http_client import exceptions
 
 
 def welcome_email(email):
-    """Sends Welcome email template."""
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 
-    message = sendgrid.Mail()
+    personalization = Personalization()
+    personalization.add_to(Email("sendgridtesting@gmail.com"))
 
-    from_email = Email("todd@hackersandslackers.com")
-    to_email = Email(email)
-    subject = "Sending with SendGrid is Fun"
-    content = Content("text/plain", "and easy to do anywhere, even with Python")
-    mail = Mail(from_email, subject, to_email, content)
+    sg = SendGridAPIClient()
+    mail = Mail()
+    mail.from_email = Email('todd@hackersandslackers.com')
+    mail.template_id = 'd-79888eacf2a74def8d2b673891b2f75a'
+    p = Personalization()
+    p.add_to(Email(email))
+    mail.add_personalization(personalization)
+
+    try:
+        response = sg.client.mail.send.post(request_body=mail.get())
+    except exceptions.BadRequestsError as e:
+        print(e.body)
+        exit()
     response = sg.client.mail.send.post(request_body=mail.get())
     print(response.status_code)
     print(response.body)
     print(response.headers)
-
-    sg.send(message)
