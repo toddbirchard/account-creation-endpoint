81233d79153228255c545ca4c721f48834e89d96
Todd Birchard <toddbirchard@gmail.com>
2018-12-02 11:59:49 -0500

    signup form error

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def subscribe(request):
     """Prepare  a new user account for the subscriber."""
     # Prepare Account Values.
     request_json = request.get_json()
-    username = request_json['username']
-    email = request_json['email']
+    username = request.form['username']
+    email = request.form['email']
     # Derive Gravatar from Email
     grav = get_gravatar(email)
     request_json['gravatar'] = grav
