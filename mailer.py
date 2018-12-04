2bbe5dcf88b08b589970cc64f33b0a9a6acfb31a
Todd Birchard <toddbirchard@gmail.com>
2018-12-04 14:12:07 -0500

    sendgrid

--- mailer.py
+++ mailer.py
@@ -4,29 +4,30 @@ from sendgrid import SendGridAPIClient
 import os
 from sendgrid.helpers.mail import Email, Substitution, Mail, Personalization
 from python_http_client import exceptions
+import datetime
+import json
 
 
-def welcome_email(email):
+def welcome_email(email_address):
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 
     sg = SendGridAPIClient()
-    mail = Mail()
-    mail.from_email = Email('toddbirchard@gmail.com')
-    mail.template_id = 'd-79888eacf2a74def8d2b673891b2f75a'
+    from_email = Email('toddbirchard@gmail.com')
+    to_email = Email(email_address)
+    subject = "Welcome to Hackers and Slackers"
+    template_id = 'd-79888eacf2a74def8d2b673891b2f75a'
 
-    p = Personalization()
-    p.add_to(Email(email))
-    p.add_cc(Email("i.am.todd.birchard@gmail.com"))
-    p.add_cc("sendgridtesting@gmail.com")
-
-    mail.add_personalization(p)
-
-    try:
-        response = sg.client.mail.send.post(request_body=mail.get())
-    except exceptions.BadRequestsError as e:
-        print(e.body)
-        exit()
+    mail = Mail(from_email, subject, to_email, template_id)
     response = sg.client.mail.send.post(request_body=mail.get())
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+    status = b"{}".decode('utf-8').format(response.status_code)
+    body = b"{}".decode('utf-8').format(response.body)
+    headers = b"{}".decode('utf-8').format(response.headers)
+    data = {
+        'status': status,
+        'body': body,
+        'headers': headers.splitlines(),
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
--- main.py
+++ main.py
@@ -9,13 +9,6 @@ def subscribe(req):
     """Prepare  a new user account for the subscriber."""
     # Prepare Account Values.
     req_json = req.get_json()
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
     username = req.form['username']
     email = req.form['email']
     grav = get_gravatar(email)   # Derive Gravatar from Email
